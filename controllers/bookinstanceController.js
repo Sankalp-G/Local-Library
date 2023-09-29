4ec0bcd63d7cfc574992d9c644302a09fbbe5da4
Sankalp-G <sankalp.gawali@gmail.com>
2023-09-29 14:11:49 +0530

    implement book instance detail

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,3 +1,4 @@
+const { Mongoose } = require("mongoose");
 const BookInstance = require("../models/bookinstance");
 const asyncHandler = require("express-async-handler");
 
@@ -13,7 +14,27 @@ exports.bookinstance_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+  if (!Mongoose.prototype.isValidObjectId(req.params.id)) {
+    const err = new Error("Invalid Book Instance ID");
+    err.status = 400;
+    return next(err);
+  }
+
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    // No results.
+    const err = new Error("Book copy not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bookinstance_detail", {
+    title: "Book:",
+    bookinstance: bookInstance,
+  });
 });
 
 // Display BookInstance create form on GET.
